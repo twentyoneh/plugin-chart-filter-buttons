2233ef59370b413f3e92a52c39f7e1468bb757b2
twentyoneh <didiwot@yandex.ru>
2026-04-08 12:57:29 +0000

    package, jest, tsconfig

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,15 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!pretty-ms|parse-ms|@superset-ui|@babel/runtime|@emotion)',
+  ],
   moduleFileExtensions: ['mock.js', 'ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     '\\.(gif|ttf|eot|png|jpg)$': '<rootDir>/test/__mocks__/mockExportString.js',
+    '^@superset-ui/(.*)$': '<rootDir>/../../node_modules/@superset-ui/$1/src',
   },
-  testEnvironment: 'jsdom',
 };
